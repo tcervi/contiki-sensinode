b4ad376b26e9a6f23291c79b3cd8d16920c8ac8a
Rede de Sensores sem Fio - UTFPR <utfprwsn@gmail.com>
2017-03-19 19:23:28 -0300

    Testing: Complete Ativ3

--- examples/cc2530dk/atividade3.c
+++ examples/cc2530dk/atividade3.c
@@ -12,31 +12,41 @@
 #include "dev/leds.h"
 
 #include <stdio.h> /* For printf() */
+# define LED_PING_EVENT 44
+# define LED_PONG_EVENT 45
 /*---------------------------------------------------------------------------*/
 static struct etimer et_hello;
 static struct etimer et_blink;
-static uint16_t count;
+static struct etimer et_proc3;
+static struct timer  t_pong;
+static uint16_t count, count2, countPong;
 static uint8_t blinks;
 /*---------------------------------------------------------------------------*/
 PROCESS(hello_world_process, "Hello world process");
 PROCESS(blink_process, "LED blink process");
-AUTOSTART_PROCESSES(&blink_process);
+PROCESS(proc3_process, "Additional process");
+PROCESS(pong_process, "Pong process");
+AUTOSTART_PROCESSES(&blink_process, &hello_world_process, &proc3_process, &pong_process);
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(hello_world_process, ev, data)
 {
   PROCESS_BEGIN();
 
-  etimer_set(&et_hello, CLOCK_SECOND * 4);
+  etimer_set(&et_hello, CLOCK_SECOND * 8);
   count = 0;
 
   while(1) {
     PROCESS_WAIT_EVENT();
-
     if(ev == PROCESS_EVENT_TIMER) {
-      printf("Hello world #%u!\n", count);
-      count++;
-
-      etimer_reset(&et_hello);
+		printf("Hello world: pinging...\n");
+		process_post(&pong_process, LED_PING_EVENT,(void*)(&hello_world_process));
+		PROCESS_WAIT_EVENT();
+		if(ev == LED_PONG_EVENT){
+			leds_toggle(LEDS_GREEN);
+			etimer_reset(&et_hello);
+			count++;
+			printf("Hello...#%u: LED Pong!\n", count);
+		}
     }
   }
 
@@ -48,23 +58,71 @@ PROCESS_THREAD(blink_process, ev, data)
   PROCESS_BEGIN();
 
   blinks = 0;
-  printf("LEDS_ALL = %d\n", LEDS_ALL);
+  etimer_set(&et_blink, CLOCK_SECOND * 2);
 
-  leds_on(LEDS_ALL);
-  etimer_set(&et_blink, 5*CLOCK_SECOND);
-  PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_TIMER);
-
-  while(1) {
-    etimer_set(&et_blink, CLOCK_SECOND);
-
-    PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_TIMER);
-
-    leds_off(LEDS_ALL);
-    leds_on(blinks & LEDS_ALL);
-    blinks++;
-    printf("Blink... (state %X)\n", leds_get());
+  while(1){
+	PROCESS_WAIT_EVENT();
+	if(ev == PROCESS_EVENT_TIMER){
+		printf("Blink: pinging...\n");
+		process_post(&pong_process, LED_PING_EVENT,(void*)(&blink_process));
+		PROCESS_WAIT_EVENT();
+		if(ev == LED_PONG_EVENT){
+			leds_toggle(LEDS_BLUE);
+			blinks++;
+			printf("Blink...#%u: LED Pong!\n", blinks);
+			etimer_reset(&et_blink);
+		}
+	}
   }
 
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(proc3_process, ev, data)
+{
+  PROCESS_BEGIN();
+
+  etimer_set(&et_proc3, CLOCK_SECOND * 4);
+  count2 = 0;
+
+  while(1) {
+    PROCESS_WAIT_EVENT();
+    if(ev == PROCESS_EVENT_TIMER) {
+		printf("Proc3: pinging...\n");
+		process_post(&pong_process, LED_PING_EVENT,(void*)(&proc3_process));
+		PROCESS_WAIT_EVENT();
+		if(ev == LED_PONG_EVENT){
+			leds_toggle(LEDS_RED);
+			count2++;
+			printf("Proc3... #%u: LED Pong! \n", count2);
+			etimer_reset(&et_proc3);
+		}
+    }
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(pong_process, ev, data)
+{
+  PROCESS_BEGIN();
+
+  countPong = 0;
+  timer_set(&t_pong, (CLOCK_SECOND * 0.2));
+
+  while(1) {
+    PROCESS_WAIT_EVENT();
+    if(ev == LED_PING_EVENT) {
+    	leds_on(LEDS_YELLOW);
+    	printf("Pong esperando 200ms ...\n", count);
+    	timer_reset(&t_pong);
+    	while(!(timer_expired(&t_pong))){}
+		leds_off(LEDS_YELLOW);
+		countPong++;
+		printf("Pong: LED Ping! Ponging num.: #%u! ...\n", countPong);
+		process_post((struct process*)data, LED_PONG_EVENT, NULL);
+    }
+ }
   PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
